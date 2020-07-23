dd55f38de4f4869b8a5b9b88b6ed96f940553254
frankdenbow <frank.denbow@gmail.com>
2020-07-23 05:31:40 -0600

    Writing pseudo code for first iteration

--- verbosity.rb
+++ verbosity.rb
@@ -1,5 +1,20 @@
 require 'sinatra'
 
 get '/' do
-  "Shut The Fuck Up Adam Nanjee"
+  #Take Text Input From User
+
+  # Loop through input, delimeter is " " "," ""-" is there a list of common delimeters to extract words?
+    # Create a set containing all words - WORDS
+    # Create frequency array with a count of how many times the words appear - COUNT
+
+  # Loop through WORDS
+    # Lookup in websters dictionary, count # of definitions
+    # Lookup in urban dictionary, count # of definitions
+    # Analyze the word for
+      # Length
+      # Uniqueness (number of synonyms, if it exists in a dictionary)
+      # type (emoji, contraction, pronoun etc)
+    # Lookup in google trends to see average usage in modern times
+
+  # Create a score based on previous analysis 
 end
